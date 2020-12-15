6f9160b061befe8c22285258fba4daca0ec2bd2b
Vencislav Petrov <venci@wetrack.com>
2020-12-15 15:19:01 +0200

    User domain model added

---
+++ SampleApp.Domain/Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace SampleApp.Domain.Models
+{
+    using SampleApp.Domain.Models.Contracts;
+    using System;
+
+    public class User : IDomainModel
+    {
+        //private readonly Guid id;
+
+        //public User(Guid id)
+        //{
+        //    if (id != Guid.Empty)
+        //        throw new ArgumentException("Empty GUIDs are not allowed.", nameof(id));
+
+        //    this.id = id;
+        //}
+
+        //public Guid Id
+        //    => this.id;
+
+        public string Email { get; set; }
+    }
+}
