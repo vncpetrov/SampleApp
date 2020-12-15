984b64269424438f9318af9337a13340ffacdf59
Vencislav Petrov <venci@wetrack.com>
2020-12-15 19:17:55 +0200

    Startup improvements

--- SampleApp.Web/Infrastructure/Extensions/ServiceCollectionExtensoins.cs
+++ SampleApp.Web/Infrastructure/Extensions/ServiceCollectionExtensoins.cs
@@ -1,7 +1,21 @@
 ﻿namespace SampleApp.Web.Infrastructure.Extensions
 {
     using Domain.Contracts;
+    using Domain.Models;
+    using DomainServices.CommandHandlers.Car.Create;
+    using DomainServices.QueryHandlers.Car.GetByUser;
+    using DomainServices.QueryHandlers.Car.GetStatisticsByUser;
+    using Microsoft.AspNetCore.Authentication.Cookies;
+    using Microsoft.AspNetCore.Mvc.Authorization;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using SqlDataAccess;
+    using SqlDataAccess.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Utils;
 
     public static class ServiceCollectionExtensoins
     {
@@ -9,10 +23,128 @@
             this IServiceCollection services)
             where TCommand : class
             where TCommandHandler : class, ICommandHandler<TCommand>
-        {  
-            services.AddScoped<ICommandHandler<TCommand>, TCommandHandler>();
+            => services.AddScoped<ICommandHandler<TCommand>, TCommandHandler>();
+
+        public static IServiceCollection AddDatabase(
+           this IServiceCollection services,
+           IConfiguration configuration)
+           => services
+                .AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        public static IServiceCollection AddIdentity(this IServiceCollection services)
+        {
+            services
+                .AddIdentity<UserEntity, RoleEntity>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+
+                    options.SignIn.RequireConfirmedEmail = false;
+                })
+                .AddEntityFrameworkStores<AppDbContext>();
 
             return services;
         }
+
+        public static IServiceCollection AddCookieAuthentication(this IServiceCollection services) 
+        {
+            services
+                .AddAuthentication(config =>
+                {
+                    config.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    config.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    config =>
+                    {
+                        config.Cookie.Name = WebConstants.IdentityCookieName;
+                        config.LoginPath = "/identity/signin";
+                        config.Cookie.HttpOnly = true;
+                    });
+
+            return services;
+        }
+
+        public static IServiceCollection AddMvcControllers(this IServiceCollection services)
+        {
+            services
+                .AddControllersWithViews(config =>
+                {
+                    config.Filters.Add(new AuthorizeFilter());
+                })
+                .AddRazorRuntimeCompilation();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserContext, AspNetUserContext>();
+
+            AddDataWriters(services);
+            AddDataReaders(services);
+            AddCommandHandlers(services);
+            AddQueryHandlers(services);
+
+            return services;
+        }
+
+        private static void AddCommandHandlers(IServiceCollection services)
+        {
+            services.RegisterCommandHandler<CreateCarCommandHandler, CreateCarCommand>();
+        }
+
+        private static void AddQueryHandlers(IServiceCollection services)
+        {
+            services.AddScoped<IQueryHandler<GetCarsByUser, IEnumerable<Car>>,
+                               GetCarsByUserQueryHandler>();
+
+            services.AddScoped<IQueryHandler<GetStatisticsByUser, GetStatisticsByUserOutputModel>,
+                               GetStatisticsByUserQueryHandler>();
+        }
+
+        private static void AddDataWriters(IServiceCollection services)
+        {
+            var dataWriterTypes = AppDomain
+                .CurrentDomain
+                .GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(t => t.GetInterfaces().Contains(typeof(IDataWriter)));
+
+            foreach (Type @interface in dataWriterTypes.Where(a => a.IsInterface))
+            {
+                Type implementation = dataWriterTypes
+                    .FirstOrDefault(x => !x.IsInterface
+                                         && x.GetInterfaces().Contains(@interface));
+
+                services.AddScoped(
+                    serviceType: @interface,
+                    implementationType: implementation);
+            }
+        }
+
+        private static void AddDataReaders(IServiceCollection services)
+        {
+            var dataReaderTypes = AppDomain
+                .CurrentDomain
+                .GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(t => t.GetInterfaces().Contains(typeof(IDataReader)));
+
+            foreach (Type @interface in dataReaderTypes.Where(a => a.IsInterface))
+            {
+                Type implementation = dataReaderTypes
+                    .FirstOrDefault(x => !x.IsInterface
+                                         && x.GetInterfaces().Contains(@interface));
+
+                services.AddScoped(
+                    serviceType: @interface,
+                    implementationType: implementation);
+            }
+        }   
     }
-} 
+}
--- SampleApp.Web/Startup.cs
+++ SampleApp.Web/Startup.cs
@@ -1,27 +1,11 @@
 namespace SampleApp.Web
 {
     using Infrastructure.Extensions;
-    using Microsoft.AspNetCore.Authentication.Cookies;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Mvc.Authorization;
-    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using SampleApp.Domain.Contracts;
-    using SampleApp.DomainServices.CommandHandlers.Car.Create;
-    using SampleApp.Web.Infrastructure;
-    using SqlDataAccess;
-    using SqlDataAccess.Entities;
-    using System;
-    using System.Linq;
-    using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
-    using Utils;
-    using SampleApp.DomainServices.QueryHandlers.Car.GetByUser;
-    using System.Collections.Generic;
-    using SampleApp.Domain.Models;
-    using SampleApp.DomainServices.QueryHandlers.Car.GetStatisticsByUser;
 
     public class Startup
     {
@@ -31,58 +15,14 @@ namespace SampleApp.Web
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
-        {
-            services
-                .AddDbContext<AppDbContext>(options =>
-                    options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
-
-            services
-                .AddIdentity<UserEntity, RoleEntity>(options =>
-                {
-                    options.Password.RequireDigit = false;
-                    options.Password.RequireLowercase = false;
-                    options.Password.RequireNonAlphanumeric = false;
-                    options.Password.RequireUppercase = false;
-
-                    options.SignIn.RequireConfirmedEmail = false;
-                })
-                .AddEntityFrameworkStores<AppDbContext>();
-
-            services
-                .AddAuthentication(config =>
-                {
-                    config.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    config.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                })
-                .AddCookie(
-                    CookieAuthenticationDefaults.AuthenticationScheme,
-                    config =>
-                    {
-                        config.Cookie.Name = WebConstants.IdentityCookieName;
-                        config.LoginPath = "/identity/signin";
-                        config.Cookie.HttpOnly = true;
-                    });
-
-            services.AddAuthorization();
-
-            services.AddTransient<IUserContext, AspNetUserContext>();
-
-            AddDataWriters(services);
-            AddDataReaders(services);
-            AddCommandHandlers(services);
-            AddQueryHandlers(services);
-
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services.AddRouting(options => options.LowercaseUrls = true);
-
-            services
-                .AddControllersWithViews(cfg =>
-                {
-                    cfg.Filters.Add(new AuthorizeFilter());
-                })
-                .AddRazorRuntimeCompilation(); 
-        }
+         => services.AddDatabase(this.Configuration)
+                    .AddIdentity()
+                    .AddCookieAuthentication()
+                    .AddAuthorization()
+                    .AddApplicationServices()
+                    .AddDatabaseDeveloperPageExceptionFilter()
+                    .AddRouting()
+                    .AddMvcControllers();
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -116,58 +56,5 @@ namespace SampleApp.Web
             });
         }
 
-        private static void AddCommandHandlers(IServiceCollection services)
-        {
-            services.RegisterCommandHandler<CreateCarCommandHandler, CreateCarCommand>();
-        }
-
-        private static void AddQueryHandlers(IServiceCollection services)
-        {
-            services.AddScoped<IQueryHandler<GetCarsByUser, IEnumerable<Car>>,
-                               GetCarsByUserQueryHandler>();
-
-            services.AddScoped<IQueryHandler<GetStatisticsByUser, GetStatisticsByUserOutputModel>,
-                               GetStatisticsByUserQueryHandler>();
-        }
-
-        private static void AddDataWriters(IServiceCollection services)
-        {
-            var dataWriterTypes = AppDomain
-                .CurrentDomain
-                .GetAssemblies()
-                .SelectMany(s => s.GetTypes())
-                .Where(t => t.GetInterfaces().Contains(typeof(IDataWriter)));
-
-            foreach (Type @interface in dataWriterTypes.Where(a => a.IsInterface))
-            {
-                Type implementation = dataWriterTypes
-                    .FirstOrDefault(x => !x.IsInterface
-                                         && x.GetInterfaces().Contains(@interface));
-
-                services.AddScoped(
-                    serviceType: @interface,
-                    implementationType: implementation);
-            }
-        }
-
-        private static void AddDataReaders(IServiceCollection services)
-        {
-            var dataReaderTypes = AppDomain
-                .CurrentDomain
-                .GetAssemblies()
-                .SelectMany(s => s.GetTypes())
-                .Where(t => t.GetInterfaces().Contains(typeof(IDataReader)));
-
-            foreach (Type @interface in dataReaderTypes.Where(a => a.IsInterface))
-            {
-                Type implementation = dataReaderTypes
-                    .FirstOrDefault(x => !x.IsInterface
-                                         && x.GetInterfaces().Contains(@interface));
-
-                services.AddScoped(
-                    serviceType: @interface,
-                    implementationType: implementation);
-            }
-        }
     }
 }
